ca60f69785e60cf7a4528058e74be1b2058e9f09
Ezra Sowden-Guzman <ezratic@pm.me>
2026-09-20 18:13:55 -0400

    add empty semantic resolution service

--- src/services/semantic-resolution/semantic-resolution.service.ts
+++ src/services/semantic-resolution/semantic-resolution.service.ts
@@ -1,1 +1,18 @@
-export class SemanticResolutionService {}
+import type { Logger } from '@/helpers/logger/logger.types'
+import type { EcsReadonlyFacade } from '@/kernel/ecs/ecs.types'
+import type { SemanticResolutionOpts } from '@/services/semantic-resolution/semantic-resolution.types'
+
+import { DefaultLogger } from '@/helpers/logger/logger'
+
+export class SemanticResolutionService {
+  #ecsFacade: EcsReadonlyFacade
+  #logger: Logger
+
+  constructor({ ecsFacade, logger }: SemanticResolutionOpts) {
+    this.#ecsFacade = ecsFacade
+
+    this.#logger = logger ?? new DefaultLogger
+  }
+
+  
+}
